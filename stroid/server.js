2f56dc72286018fa5b91fbcf0b34401c94cf0b08
treylitefm <griffin.omar@gmail.com>
2016-11-15 15:02:36 -0800

    First commit
    Signed-off-by: treylitefm <griffin.omar@gmail.com>

---
+++ stroid/server.js
@@ -0,0 +1,12 @@
+var bp = require('body-parser')
+var express = require('express')
+var path = require('path')
+var port = 8080
+var app = express()
+
+app.use(express.static(path.join(__dirname, './client')))
+app.use(express.static(path.join(__dirname, './bower_components')))
+
+app.listen(port, function() {
+    console.log('Node server on port',port)
+})
