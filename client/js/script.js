25233a6d9497426317ba6863436ebf06daa37a6e
james-moreno <bonsaimo7@gmail.com>
2016-11-17 13:57:19 -0800

    somewhat finished physics for planets

--- client/js/script.js
+++ client/js/script.js
@@ -1,10 +1,12 @@
 class Circle {
-    constructor(r, x, y, vx, vy) {
-        this.r = r
-        this.x = x
-        this.y = y
-        this.vx = vx
-        this.vy = vy
+    constructor(r, x, y, vx, vy, up, right) {
+        this.r = r;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.up = up;
+        this.right = right;
     }
 }
 
@@ -27,57 +29,79 @@ function upTheta(){
     }
 }
 function horizontalMovement(circle){
-    if(circle.r + circle.x >= 600){
-        horizontal = false;
+    if(circle.r + circle.x >= 2400 || -circle.r + circle.x <= 0){
+        circle.vx = -circle.vx;
     }
-    else if(-circle.r + circle.x <= 0){
-        horizontal = true;
-    }
-    if(circle.r + circle.x <= 600 && horizontal === true){
+    if(circle.r + circle.x <= 2400 || circle.r + circle.x >= 0){
         circle.x += circle.vx;
     }
-    else if(circle.r + circle.x >= 0 && horizontal === false){
-        circle.x -= circle.vx;
-    }
-    return circle.x;
 }
 function verticalMovement(circle){
-    if(circle.r + circle.y >= 600){
-        vertical = false;
+    if(circle.r + circle.y >= 2400 || -circle.r + circle.y <= 0){
+        circle.vy = -circle.vy;
     }
-    else if(-circle.r + circle.y <= 0){
-        vertical = true;
-    }
-    if(circle.r + circle.y <= 600 && vertical === true){
+    if(circle.r + circle.y <= 2400 || circle.r + circle.y >= 0){
         circle.y += circle.vy;
     }
-    else if(circle.r + circle.y >= 0 && vertical === false){
-        circle.y -= circle.vy;
-    }
-    return circle.y;
 }
 
-var circles = []
+var circles = [];
 
-var vertical = true;
-var horizontal = true;
-circles.push(new Circle(50, 100, 75, 6, 6))
-circles.push(new Circle(50, 250, 75, 2, 8))
+circles.push(new Circle(50, 250, 450, 4, 3));
+circles.push(new Circle(50, 250, 75, 3, 5));
+circles.push(new Circle(50, 300, 275, 3, 2));
+circles.push(new Circle(50, 51, 1175, 2, 4));
+circles.push(new Circle(50, 1250, 450, 5, 2));
+circles.push(new Circle(50, 250, 575, 6, 1));
+circles.push(new Circle(50, 1300, 275, 3, 1));
+circles.push(new Circle(50, 700, 375, 2, 3));
+circles.push(new Circle(50, 1250, 750, 2, 1));
+circles.push(new Circle(50, 550, 1375, 1, 4));
+circles.push(new Circle(50, 300, 1275, 1, 2));
+circles.push(new Circle(50, 51, 75, 2, 1));
 var thetaA = 0;
 var thetaB = 0;
 var c=document.getElementById("stage");
 var ctx=c.getContext("2d");
 
 window.requestAnimationFrame(function() {
-    updatePosition(thetaB, thetaA)
-})
+    updatePosition(thetaB, thetaA);
+});
+
+function collision(circleA, circleB){
+    var dx = circleA.x - circleB.x;
+    var dy = circleA.y - circleB.y;
+    var distance = Math.sqrt(dx * dx + dy * dy);
+    var tmp;
+    if (distance < circleA.r + circleB.r){
+        if((circleA.vy > 0 && circleB.vy < 0) || (circleA.vy < 0 && circleB.vy > 0)){
+            tmp = circleA.vy;
+            circleA.vy = circleB.vy;
+            circleB.vy = tmp;
+        }
+        if((circleA.vy > 0 && circleB.vy > 0) || (circleA.vy < 0 && circleB.vy < 0)){
+            tmp = circleA.vy;
+            circleA.vy = circleB.vy;
+            circleB.vy = tmp;
+        }
+        if((circleA.vx > 0 && circleB.vx < 0) || (circleA.vx < 0 && circleB.vx > 0)){
+            tmp = circleA.vx;
+            circleA.vx = circleB.vx;
+            circleB.vx = tmp;
+        }
+        if((circleA.vx > 0 && circleB.vx > 0) || (circleA.vx < 0 && circleB.vx < 0)){
+            tmp = circleA.vx;
+            circleA.vx = circleB.vx;
+            circleB.vx = tmp;
+        }
+    }
+}
 
 function updatePosition(valA, valB){
-    ctx.clearRect(0, 0, 600, 600)
+    ctx.clearRect(0, 0, 2400, 2400);
     upTheta();
 
     for (c in circles) {
-        //console.log(circles[c].x,circles[c].y,circles[c].r)
         horizontalMovement(circles[c]);
         verticalMovement(circles[c]);
 
@@ -85,14 +109,20 @@ function updatePosition(valA, valB){
         ctx.arc(circles[c].x,circles[c].y,circles[c].r,0,2*Math.PI);
         ctx.stroke();
 
-        var b = point(circles[c].r+10, -valA, {x:circles[c].x,y:circles[c].y});
+        var b = point(circles[c].r+10, valB, {x:circles[c].x,y:circles[c].y});
 
         ctx.beginPath();
         ctx.arc(b.x,b.y,10,0,2*Math.PI);
         ctx.stroke();
-        //console.log(b)
+    }
+    for (var i = 0; i < circles.length-1; i++) {
+        for (var j = i+1; j < circles.length; j++) {
+            if (i !== j) {
+                collision(circles[i], circles[j]);
+            }
+        }
     }
     window.requestAnimationFrame(function() {
-        updatePosition(thetaB, thetaA)
-    })
+        updatePosition(thetaB, thetaA);
+    });
 }
