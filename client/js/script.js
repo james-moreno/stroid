a6fb2cdbf97ef1fef6237888214644915b054e5f
james-moreno <bonsaimo7@gmail.com>
2016-11-21 11:45:14 -0800

    obreezy added player class

--- client/js/script.js
+++ client/js/script.js
@@ -5,8 +5,30 @@ class Circle {
         this.y = y;
         this.vx = vx;
         this.vy = vy;
-        this.up = up;
-        this.right = right;
+        this.players = []
+    }
+}
+class Player {
+    constructor(data){
+        this.x = data.x;
+        this.y = data.y;
+        this.hitbox = 5;
+        this.theta = data.theta;
+    }
+
+    updateTheta(){
+        if(this.theta > Math.PI*2){
+            this.theta = 0;
+        }
+        else {
+            this.theta += 0.0174533;
+            //thetaB += (0.0174533*2);
+        }
+    }
+
+    updatePosition(point) {
+        this.x = point.x;
+        this.y = point.y;
     }
 }
 
@@ -16,31 +38,20 @@ function point(r, angle, center) {
         y: r*Math.sin(angle)+center.y
     };
 }
-function upTheta(){
-    if(thetaA > Math.PI*2){
-        thetaA = 0;
-    }
-    else if(thetaB > Math.PI*2){
-        thetaB = 0;
-    }
-    else {
-        thetaA += 0.0174533;
-        thetaB += (0.0174533*2);
-    }
-}
+
 function horizontalMovement(circle){
-    if(circle.r + circle.x >= 2400 || -circle.r + circle.x <= 0){
+    if(circle.r + circle.x >= 800 || -circle.r + circle.x <= 0){
         circle.vx = -circle.vx;
     }
-    if(circle.r + circle.x <= 2400 || circle.r + circle.x >= 0){
+    if(circle.r + circle.x <= 800 || circle.r + circle.x >= 0){
         circle.x += circle.vx;
     }
 }
 function verticalMovement(circle){
-    if(circle.r + circle.y >= 2400 || -circle.r + circle.y <= 0){
+    if(circle.r + circle.y >= 800 || -circle.r + circle.y <= 0){
         circle.vy = -circle.vy;
     }
-    if(circle.r + circle.y <= 2400 || circle.r + circle.y >= 0){
+    if(circle.r + circle.y <= 800 || circle.r + circle.y >= 0){
         circle.y += circle.vy;
     }
 }
@@ -50,19 +61,16 @@ var circles = [];
 circles.push(new Circle(50, 250, 450, 4, 3));
 circles.push(new Circle(50, 250, 75, 3, 5));
 circles.push(new Circle(50, 300, 275, 3, 2));
-circles.push(new Circle(50, 51, 1175, 2, 4));
-circles.push(new Circle(50, 1250, 450, 5, 2));
-circles.push(new Circle(50, 250, 575, 6, 1));
-circles.push(new Circle(50, 1300, 275, 3, 1));
-circles.push(new Circle(50, 700, 375, 2, 3));
-circles.push(new Circle(50, 1250, 750, 2, 1));
-circles.push(new Circle(50, 550, 1375, 1, 4));
-circles.push(new Circle(50, 300, 1275, 1, 2));
-circles.push(new Circle(50, 51, 75, 2, 1));
 var thetaA = 0;
 var thetaB = 0;
 var c=document.getElementById("stage");
 var ctx=c.getContext("2d");
+
+circles[0].players.push(new Player({theta:0}));
+circles[0].players.push(new Player({theta:1}));
+circles[0].players.push(new Player({theta:2}));
+
+// var player = new Circle(5, )
 
 window.requestAnimationFrame(function() {
     updatePosition(thetaB, thetaA);
@@ -99,7 +107,6 @@ function collision(circleA, circleB){
 
 function updatePosition(valA, valB){
     ctx.clearRect(0, 0, 2400, 2400);
-    upTheta();
 
     for (c in circles) {
         horizontalMovement(circles[c]);
@@ -109,11 +116,16 @@ function updatePosition(valA, valB){
         ctx.arc(circles[c].x,circles[c].y,circles[c].r,0,2*Math.PI);
         ctx.stroke();
 
-        var b = point(circles[c].r+10, valB, {x:circles[c].x,y:circles[c].y});
+        for (var p in circles[c].players) {
+            var pointOnOuterCircle = point(circles[c].r+10, circles[c].players[p].theta, {x:circles[c].x,y:circles[c].y});
 
-        ctx.beginPath();
-        ctx.arc(b.x,b.y,10,0,2*Math.PI);
-        ctx.stroke();
+            ctx.beginPath();
+            ctx.arc(pointOnOuterCircle.x,pointOnOuterCircle.y,10,0,2*Math.PI);
+            ctx.stroke();
+
+            circles[c].players[p].updateTheta();
+            circles[c].players[p].updatePosition(pointOnOuterCircle);
+        }
     }
     for (var i = 0; i < circles.length-1; i++) {
         for (var j = i+1; j < circles.length; j++) {
@@ -122,6 +134,7 @@ function updatePosition(valA, valB){
             }
         }
     }
+
     window.requestAnimationFrame(function() {
         updatePosition(thetaB, thetaA);
     });
